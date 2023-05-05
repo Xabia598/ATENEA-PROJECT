6ad0195525e65a0d7e820050df6bfa07f14fb191
Xabia598 <xabia572@gmail.com>
2023-05-05 20:55:16 +0200

    Implementacion query

---
+++ _query.py
@@ -0,0 +1,47 @@
+import sqlite3
+
+print("CTRL + C TO EXIT")
+
+conn = sqlite3.connect('atenea.db')
+
+c = conn.cursor()
+
+mode = input("Country | City | IP :: ")
+
+if mode == "Country":
+
+    def search():
+
+        target = input("Enter Country Name (In English) :: ")
+        cursor = conn.execute(f"SELECT * FROM Atenea WHERE Country = '{target}'")
+
+        for row in cursor:
+            print(row)
+
+    while True:
+        search()
+    
+else:
+    if mode == "City":
+        def search():
+
+            target = input("Enter City Name (In English) :: ")
+            cursor = conn.execute(f"SELECT * FROM Atenea WHERE City = '{target}'")
+
+            for row in cursor:
+                print(row)
+
+        while True:
+            search()
+    
+    else:
+        def search():
+
+            target = input("Enter IP :: ")
+            cursor = conn.execute(f"SELECT * FROM Atenea WHERE IP = '{target}'")
+
+            for row in cursor:
+                print(row)
+
+        while True:
+            search()
